d59520e75e10f3c9e508075f5c46ddc514311af7
caamlg <caamila.lg@gmail.com>
2021-10-06 21:20:44 -0300

    agrego navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+export const NavBar = () => {
+    return (
+        <nav className="nav">
+            <div className="brand">viajando ✈</div>
+            <div id="mainListDiv" className="main_list">
+            <ul className="navlinks">
+                <li><a href="#">Organización</a></li>
+                <li><a href="#">Cuadernos</a></li>
+                <li><a href="#">Deco y regalos</a></li>
+                <li><a href="#">Accesorios</a></li>
+                <li><a href="#">Mochilas y valijas</a></li>
+            </ul>
+            </div>
+        </nav>
+    )
+}
