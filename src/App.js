0b0112b396fbc8b487822a1c261588efb4155ef0
caamlg <caamila.lg@gmail.com>
2021-10-14 15:58:40 -0300

    responsive navbar ok

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { ItemCount } from './components/ItemCount';
 import { ItemListContainer } from './components/ItemListContainer';
-import { NavBar } from './components/NavBar';
+import { NavBar } from './components/NavBar.js';
 
 function App() {
   return (
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,22 @@ import {CartWidget} from './CartWidget.js'
 import './NavBar.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import {useState} from 'react'
 
 export const NavBar = () => {
+
+    const [open, setOpen] = useState(false);
+
+    const barsIcon = <FontAwesomeIcon icon={faBars} className="hamburger" onClick={() => setOpen(!open)}/>
+
+    const closeIcon = <FontAwesomeIcon icon={faTimes} className="hamburger" onClick={() => setOpen(!open)}/>
  
     return (
         <nav className="nav">
             <div className="brand">viajando ✈</div>
-            <FontAwesomeIcon icon={faBars} className="hamburger" />
-            <div id="mainListDiv" className="main_list">
+            {open ? closeIcon : barsIcon}
+            <div id="mainListDiv" className={open ? "main_list--open" : "main_list"}>
             <ul className="navlinks">
                 <li><a href="/#">Organización</a></li>
                 <li><a href="/#">Cuadernos</a></li>
