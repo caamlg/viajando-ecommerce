72286a00547f62e30a04b5e4a9e7f8fa80ba6354
caamlg <caamila.lg@gmail.com>
2021-10-19 15:41:59 -0300

    correccion ItemListContainer

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,11 +1,32 @@
 import React from "react";
 import { ItemList } from "./ItemList";
 import "./ItemListContainer.css";
+import { useEffect, useState } from "react";
+import Products from "./Products.json";
 
 export const ItemListContainer = (props) => {
+  const [items, setItems] = useState([]);
+
+  const getData = (data) =>
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (data) {
+          resolve(data);
+        } else {
+          reject("No se encontró nada");
+        }
+      }, 2000);
+    });
+
+  useEffect(() => {
+    getData(Products)
+      .then((result) => setItems(result))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <>
-      <ItemList/>
+      <ItemList items={items} />
     </>
   );
 };
